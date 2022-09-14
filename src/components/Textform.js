d43e1744eedb3a5f3c5aab13cdcd47a7db436c40
Ghanshyam <mistry7373@gmail.com>
2022-09-14 19:45:39 +0530

    fix some bugs

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -17,21 +17,17 @@ function Textform(props) {
         setText(event.target.value)
     }
     const handleCopy = () =>{
-        let text = document.getElementById('databox');
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert('Copied to Clipboard');
     }
     const handleExtraSpace = () =>{
         let newtext = text.split(/[ ]+/);
         setText(newtext.join(" "));
     }
-    const countWords = (s) => {
-        s = s.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
-        s = s.replace(/[ ]{2,}/gi," ");//2 or more space to 1
-        s = s.replace(/\n /,"\n"); // exclude newline with a start spacing
-        return s.split(' ').filter(function(str){return str!=="";}).length;
-      }
+    const handleLineSpace = () =>{
+        let newtext = text.split(/\s+/);
+        setText(newtext.join(" "));
+    }
     const [text, setText] = useState("")
 
     return (
@@ -41,19 +37,20 @@ function Textform(props) {
                 <div className="mb-3">
                     <textarea className="form-control" placeholder="Enter your text" value={text} onChange={handleonChange} id="databox" rows="12" style={{backgroundColor: props.mode === 'light'?'white':'black',color:props.mode === 'light'?'black':'white'}}></textarea>
                 </div>
-                <button className="btn btn-info mx-2" id="toupper" onClick={handletoUpper}>Convert to Uppercase</button>
-                <button className="btn btn-info mx-2" id="toLower" onClick={handletoLower}>Convert to Lowercase</button>
-                <button className="btn btn-info mx-2" id="toLower" onClick={handleclearText}>Clear Text</button>
-                <button className="btn btn-info mx-2" id="copytext" onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-info mx-2" id="removeExtraSpace" onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="toupper" onClick={handletoUpper}>Convert to Uppercase</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="toLower" onClick={handletoLower}>Convert to Lowercase</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="toLower" onClick={handleclearText}>Clear Text</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="copytext" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="removeExtraSpace" onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="removeLineSpace" onClick={handleLineSpace}>Remove NewLine Space</button>
             </div>
             <hr />
             <div className="container my-3">
                 <h1>Your Summary</h1>
-                <p>{countWords(text)} Words & {text.length} Characters</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Words & {text.length} Characters</p>
                 <p>{(text.length ===0)?0: 0.008 * text.split(" ").length} Minutes for reading your content</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length > 0?text:'Nothing to Preview'}</p>
             </div>
         </div>
     )
