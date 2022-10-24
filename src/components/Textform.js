c4b25e2689ac26d0cbc616c89051cbc77fe9f33f
Ghanshyam <mistry7373@gmail.com>
2022-10-24 20:12:23 +0530

    Added Para-phrasing

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,6 +1,30 @@
 import React, { useState } from 'react'
 
 function Textform(props) {
+    const [text, setText] = useState("")
+
+    const handleParaPhasing = () => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '12fd4790e1mshdd9e9bed5f94427p130f9djsn259398c99e21',
+                'X-RapidAPI-Host': 'rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com'
+            },
+            body:`{"language":"en","strength":3,"text":${JSON.stringify(text)}}`
+        };
+
+        fetch('https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite', options)
+            .then(response => response.json())
+            .then(response => {
+                // console.log(response)
+                setText(response.rewrite)
+                props.showAlert('Text Para-pharsing Successfull!')}
+            )
+            .catch(err => console.error(err));
+
+    }
+
 
     const handletoUpper = () => {
         let upper = text.toUpperCase()
@@ -16,26 +40,25 @@ function Textform(props) {
     const handleonChange = (event) => {
         setText(event.target.value)
     }
-    const handleCopy = () =>{
+    const handleCopy = () => {
         navigator.clipboard.writeText(text);
         props.showAlert('Copied to Clipboard');
     }
-    const handleExtraSpace = () =>{
+    const handleExtraSpace = () => {
         let newtext = text.split(/[ ]+/);
         setText(newtext.join(" "));
     }
-    const handleLineSpace = () =>{
+    const handleLineSpace = () => {
         let newtext = text.split(/\s+/);
         setText(newtext.join(" "));
     }
-    const [text, setText] = useState("")
 
     return (
-        <div style={{color:props.mode === 'light'?'black':'white'}}>
+        <div style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
             <div className='container my-3'>
                 <h1>Enter your text to analyze</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" placeholder="Enter your text" value={text} onChange={handleonChange} id="databox" rows="12" style={{backgroundColor: props.mode === 'light'?'white':'black',color:props.mode === 'light'?'black':'white'}}></textarea>
+                    <textarea className="form-control" placeholder="Enter your text" value={text} onChange={handleonChange} id="databox" rows="12" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', color: props.mode === 'light' ? 'black' : 'white' }}></textarea>
                 </div>
                 <button disabled={text.length === 0} className="btn btn-info mx-2" id="toupper" onClick={handletoUpper}>Convert to Uppercase</button>
                 <button disabled={text.length === 0} className="btn btn-info mx-2" id="toLower" onClick={handletoLower}>Convert to Lowercase</button>
@@ -43,14 +66,15 @@ function Textform(props) {
                 <button disabled={text.length === 0} className="btn btn-info mx-2" id="copytext" onClick={handleCopy}>Copy Text</button>
                 <button disabled={text.length === 0} className="btn btn-info mx-2" id="removeExtraSpace" onClick={handleExtraSpace}>Remove Extra Space</button>
                 <button disabled={text.length === 0} className="btn btn-info mx-2" id="removeLineSpace" onClick={handleLineSpace}>Remove NewLine Space</button>
+                <button disabled={text.length === 0} className="btn btn-info mx-2" id="paraphasing" onClick={handleParaPhasing}>Para Phasing</button>
             </div>
             <hr />
             <div className="container my-3">
                 <h1>Your Summary</h1>
-                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Words & {text.length} Characters</p>
-                <p>{(text.length ===0)?0: 0.008 * text.split(" ").length} Minutes for reading your content</p>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Words & {text.length} Characters</p>
+                <p>{(text.length === 0) ? 0 : 0.008 * text.split(" ").length} Minutes for reading your content</p>
                 <h2>Preview</h2>
-                <p>{text.length > 0?text:'Nothing to Preview'}</p>
+                <p>{text.length > 0 ? text : 'Nothing to Preview'}</p>
             </div>
         </div>
     )
